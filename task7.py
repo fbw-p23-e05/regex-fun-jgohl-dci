20c7f363e4e13aeb7b45f9aa95918196a5bb8ff4
jgohl-dci <johannes.gohl@dci-student.org>
2023-09-07 11:42:00 +0200

    ed 7

--- task7.py
+++ task7.py
@@ -1,5 +1,5 @@
 import re
 
-text = "ccccccabbbc HHhhhhh_"
+text = "ccccccabbbc HHhhhhh_gg"
 
-print(re.search("[a-z]\_", text))
+print(re.search("[a-z]+_[a-z]*", text))     #lower, underline and maybe more lower
